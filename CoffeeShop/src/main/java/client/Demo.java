d5f69fc3a039b9e4fdfdaa8c57c5cc0080a39afe
CamyH <cameronhunt88@hotmail.com>
2025-02-14 17:14:09 +0000

    Add initial maven project files

---
+++ CoffeeShop/src/main/java/client/Demo.java
@@ -0,0 +1,11 @@
+package client;
+
+/**
+ * Demo initialises the CoffeeShop program
+ * Contains the main method
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
