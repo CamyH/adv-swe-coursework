f1c0f33c267c7cd5029e1974355b7b046a4b60f7
CamyH <cameronhunt88@hotmail.com>
2025-02-19 11:40:25 +0000

    Add Discount enum with discounts

---
+++ CoffeeShop/src/main/java/utils/Discount.java
@@ -0,0 +1,15 @@
+package utils;
+
+/**
+ * Calculates the discount for the current price
+ */
+public enum Discount {
+    DISCOUNT10(10),
+    DISCOUNT25(25),
+    DISCOUNT50(50),
+    DISCOUNT75(75);
+
+    private final int discount;
+
+    Discount(int discount) { this.discount = discount; }
+}
