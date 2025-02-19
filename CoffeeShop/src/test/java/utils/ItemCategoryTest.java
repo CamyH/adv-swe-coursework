45206b2350bf978817fec83922e0aad0217ada2e
CamyH <cameronhunt88@hotmail.com>
2025-02-19 15:06:27 +0000

    Add simple test to ensure the values are correct

---
+++ CoffeeShop/src/test/java/utils/ItemCategoryTest.java
@@ -0,0 +1,19 @@
+package utils;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for the ItemCategory enum
+ */
+public class ItemCategoryTest {
+    @Test
+    void testEnumValues() {
+        assertEquals("Roll", ItemCategory.ROLL.getName());
+        assertEquals("Food", ItemCategory.FOOD.getName());
+        assertEquals("HotDrink", ItemCategory.HOTDRINK.getName());
+        assertEquals("SoftDrink", ItemCategory.SOFTDRINK.getName());
+        assertEquals("Snack", ItemCategory.SNACK.getName());
+        assertEquals("Pastry", ItemCategory.PASTRY.getName());
+    }
+}
