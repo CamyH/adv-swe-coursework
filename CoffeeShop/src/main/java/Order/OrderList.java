45d42c31d439f1eba17632ac7a1cbe817a9c9556
Fraser Holman <fraserholman1@gmail.com>
2025-02-20 16:09:21 +0000

    Created empty OrderList methods

--- CoffeeShop/src/main/java/Order/OrderList.java
+++ CoffeeShop/src/main/java/Order/OrderList.java
@@ -2,7 +2,12 @@ package Order;
 
 import Interface.EntityList;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class OrderList implements EntityList {
+    private Queue<Object> queue = new ArrayDeque<>();
+
     public OrderList() {}
 
     @Override
@@ -13,5 +18,17 @@ public class OrderList implements EntityList {
     @Override
     public void remove(Object ID) {
 
+    }
+
+    public Object getOrder(String orderID) {
+        return null;
+    }
+
+    public Queue<Object> getOrderList() {
+        return queue;
+    }
+
+    public String getOrderDetails() {
+        return "";
     }
 }
