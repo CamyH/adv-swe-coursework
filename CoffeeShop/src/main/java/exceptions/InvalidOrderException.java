ae7b765166b024395d37664a711c083f9947edfc
faiz-k4 <faiz.mohd340@gmail.com>
2025-02-26 10:17:10 +0000

    Defining Exception package Putting Invalid Order Exception class to work on later

---
+++ CoffeeShop/src/main/java/exceptions/InvalidOrderException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+/**
+ * Throws an error if there is any invalid order.
+ *
+ * @author Mohd Faiz
+ */
+public class InvalidOrderException {
+}
---
+++ CoffeeShop/src/test/java/exceptions/InvalidOrderExceptionTest.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+/**
+ * Unit test for Invalid Orders Exception class.
+ *
+ * @author Mohd Faiz
+ */
+public class InvalidOrderExceptionTest {
+}
