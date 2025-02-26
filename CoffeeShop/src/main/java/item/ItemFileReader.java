0d9f1be2a8c37d08e84c9bfb0a89ab5bf9478071
CamyH <cameronhunt88@hotmail.com>
2025-02-26 12:12:06 +0000

    Add ingest method to create list of items

--- CoffeeShop/src/main/java/item/ItemFileReader.java
+++ CoffeeShop/src/main/java/item/ItemFileReader.java
@@ -22,13 +22,13 @@ public class ItemFileReader implements FileManager<Item> {
     @Override
     public ArrayList<Item> readFile() throws IOException {
         File itemFile = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
 
         // Throw exception early if file does not exist
         if (!itemFile.exists()) throw new FileNotFoundException();
 
         try (FileInputStream fis = new FileInputStream(itemFile)) {
             BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
-            StringBuilder stringBuilder = new StringBuilder();
             String line;
             while ((line = reader.readLine()) != null) {
                 stringBuilder.append(line);
@@ -38,17 +38,36 @@ public class ItemFileReader implements FileManager<Item> {
             System.out.println(e.getMessage());
         }
 
-        return null;
+        return ingestFileContents(stringBuilder);
     }
 
     /**
      * Write to a given file
-     *
      * @param report all order information to be used for reporting
      */
     @Override
     public void writeToFile(Item report) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    /**
+     * Convert StringBuilder file content to a list of item objects
+     * @param fileContents the contents of the read from file
+     * @return an ArrayList of type item
+     */
+    private ArrayList<Item> ingestFileContents(StringBuilder fileContents) {
+        ArrayList<Item> items = new ArrayList<>();
+
+        for (String line : fileContents.toString().split("\n")) {
+            // Skip empty lines
+            if (line.trim().isEmpty()) continue;
+
+            String[] lineData = line.split(",");
+
+            items.add(new Item(lineData[0], ItemCategory.valueOf(lineData[1]), Double.parseDouble(lineData[2]), lineData[3]));
+        }
+
+        return items;
     }
 
     /**
