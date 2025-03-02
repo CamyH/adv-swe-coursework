04f9a8e1e699cb37f57c80cbf13ed1d23f3ef666
Caelan Mackenzie <cm2056@hw.ac.uk>
2025-03-02 20:47:35 +0000

    Changed console to use switch for commands

--- CoffeeShop/src/main/java/client/Console.java
+++ CoffeeShop/src/main/java/client/Console.java
@@ -29,25 +29,70 @@ public class Console {
         System.out.println("Coffee Shop Console");
         System.out.println("Type 'help' for a list of commands.");
 
-        String command;
-        do{
+        while (true){
             System.out.println("Enter your command:");
-            command = scanner.nextLine();
-            if(command.equals("add")){
-                addToMenu();
+            String command = scanner.nextLine();
+            switch (command) {
+                case "addToMenu": {
+                    addToMenu();
+                    break;
+                }
+                case "removeFromMenu": {
+                    removeFromMenu();
+                    break;
+                }
+                case "newOrder": {
+                    newOrder();
+                    break;
+                }
+                case "addItem": {
+                    addItem();
+                    break;
+                }
+                case "placeOrder": {
+                    placeOrder();
+                    break;
+                }
+                case "viewOrderList": {
+                    viewOrderList();
+                    break;
+                }
+                case "help": {
+                    System.out.println("newOrder, viewOrderList, addToMenu, removeFromMenu, quit");
+                    break;
+                }
+                case "quit": {
+                    System.out.println("Goodbye.");
+                    return;
+                }
+                default: {
+                    System.out.println("Unknown command. Type 'help' for a list of commands.");
+                }
             }
-            else if(command.equals("help")){
-                help();
-            }
-        } while(!(command.equals("quit")));
-        System.out.println("Goodbye.");
+        }
     }
 
     private void addToMenu() {
 
     }
 
-    private void help() {
-        System.out.println("add, help, quit");
+    private void removeFromMenu() {
+
+    }
+
+    private void newOrder() {
+        System.out.println("Enter the command 'addItem' to add an item to the order, then enter 'placeOrder' to place an order.");
+    }
+
+    private void addItem() {
+
+    }
+
+    private void placeOrder() {
+
+    }
+
+    private void viewOrderList() {
+
     }
 }
