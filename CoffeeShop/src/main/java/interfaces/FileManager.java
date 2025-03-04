6654e5ce15de7f01e1e9e9b0a640ec25caadead9
CamyH <cameronhunt88@hotmail.com>
2025-03-04 17:20:01 +0000

    Add autocloseable as it is used by tests

--- CoffeeShop/src/main/java/interfaces/FileManager.java
+++ CoffeeShop/src/main/java/interfaces/FileManager.java
@@ -6,9 +6,10 @@ import java.util.Collection;
 /**
  * Defines contracts for reading to and writing from a file.
  * Use of Stream is recommended as the interface extends AutoCloseable
+ * So far AutoCloseable is only used in the tests for cleanup
  * @author Cameron Hunt
  */
-public interface FileManager<T, R> {
+public interface FileManager<T, R> extends AutoCloseable {
     /**
      * Reads from a given file
      * @return an instance of type T representing the file content
