b27664cb28b1d6a78d4ab9831a559e4120fd23f1
CamyH <cameronhunt88@hotmail.com>
2025-03-04 16:54:50 +0000

    Add generate report method

---
+++ CoffeeShop/src/main/java/utils/GenerateReportFileWriter.java
@@ -0,0 +1,58 @@
+package utils;
+
+import interfaces.FileManager;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Generates a report of all information from that day
+ * Run on program exit
+ * @author Cameron Hunt
+ */
+public class GenerateReportFileWriter implements FileManager<Object, HashMap<String, Double>> {
+    private final String fileName;
+
+    GenerateReportFileWriter(String fileName) {
+        this.fileName = fileName;
+    }
+
+    /**
+     * Read the file (this method is not supported yet)
+     *
+     * @return an Object, as expected by the interface
+     */
+    @Override
+    public Collection<Object> readFile() throws IOException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    /**
+     * Write the end of program report to the given file
+     * @param items all item information to be written to the file
+     */
+    @Override
+    public void writeToFile(HashMap<String, Double> items) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
+            for (Map.Entry<String, Double> item : items.entrySet()) {
+                writer.write(item.getKey() + "\t" + item.getValue() + "\n");
+            }
+        }
+    }
+
+    /**
+     * Closes this resource, relinquishing any underlying resources.
+     * This method is invoked automatically on objects managed by the
+     * {@code try}-with-resources statement.
+     *
+     * @throws Exception if this resource cannot be closed
+     */
+    @Override
+    public void close() throws Exception {
+
+    }
+}
