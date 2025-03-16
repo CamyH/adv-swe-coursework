699bc06bf8aec69d4a4bf25342532614df87e363
CamyH <cameronhunt88@hotmail.com>
2025-03-16 16:28:41 +0000

    Refactor Message to a record

--- CoffeeShop/src/main/java/Message/Message.java
+++ CoffeeShop/src/main/java/Message/Message.java
@@ -3,60 +3,21 @@ package Message;
 import java.util.UUID;
 
 /**
- * Represents a message with a specific type and sender.
+ * Represents a message with a specific type and sender
  */
-public class Message {
-    private final String message;
-    private final MessageType messageType;
-    private final UUID customerId;
+public record Message(String message, MessageType messageType, UUID customerId) {
 
     /**
-     * Constructs a Message with the specified details.
+     * Returns a string representation of the message, including message content,
+     * type, and customer Id.
      *
-     * @param message The content of the message.
-     * @param messageType The type of the message.
-     * @param customerId The ID of the customer sending the message.
-     */
-    public Message(String message, MessageType messageType, UUID customerId) {
-        this.message = message;
-        this.messageType = messageType;
-        this.customerId = customerId;
-    }
-
-    /**
-     * Returns the content of the message.
-     * @return The message content.
-     */
-    public String getMessage() {
-        return message;
-    }
-
-    /**
-     * Returns the type of the message.
-     * @return The message type.
-     */
-    public MessageType getMessageType() {
-        return messageType;
-    }
-
-    /**
-     * Returns the customer ID associated with the message.
-     * @return The customer ID.
-     */
-    public UUID getCustomerId() {
-        return customerId;
-    }
-
-    /**
-     * Provides a string representation of the message, including message content,
-     * type, and customer ID.
      * @return A string representation of the message.
      */
     @Override
     public String toString() {
         return "Message{" +
-               "message='" + message + '\'' +
-               ", messageType=" + messageType +
-               ", customerId=" + customerId + '}';
+                "message='" + message + '\'' +
+                ", messageType=" + messageType +
+                ", customerId=" + customerId + '}';
     }
 }
