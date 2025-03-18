7bd3b06f9766e262441f547e67a37cbc4a7d3e50
CamyH <cameronhunt88@hotmail.com>
2025-03-18 15:23:38 +0000

    Add server tests

---
+++ CoffeeShop/src/test/java/server/ServerTest.java
@@ -0,0 +1,58 @@
+package server;
+
+import item.ItemList;
+import order.OrderList;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.PipedOutputStream;
+import java.io.PipedInputStream;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ServerTest {
+    private static Server server;
+
+    @BeforeAll
+    public static void setup() {
+        server = new Server();
+    }
+
+    @Test
+    public void testInitialiseOrdersFromFile() {
+        server.initialiseItemsFromFile();
+        server.initialiseOrdersFromFile();
+
+        OrderList orderList = OrderList.getInstance();
+        assertNotNull(orderList, "OrderList should not be null");
+        assertFalse(orderList.getOrderList().isEmpty(), "OrderList should not be empty");
+    }
+
+    @Test
+    public void testInitialiseItemsFromFile() {
+        server.initialiseItemsFromFile();
+
+        ItemList itemList = ItemList.getInstance();
+        assertNotNull(itemList, "ItemList should not be null");
+        assertFalse(itemList.getMenu().isEmpty(), "ItemList should not be empty");
+    }
+
+    @Test
+    public void testGetConnectionSingletonInstanceIsNotNull() throws IOException {
+        CopyOnWriteArraySet<ObjectOutputStream> connectionsSingleton = Server.getConnectionsSingletonInstance();
+
+        assertNotNull(connectionsSingleton, "ConnectionsSingleton instance should not be null");
+
+        PipedOutputStream mockSocketOutput = new PipedOutputStream();
+        // Not used but the output and input popes must be connected for output to work
+        PipedInputStream mockSocketInput = new PipedInputStream(mockSocketOutput);
+        ObjectOutputStream mockOutputStream = new ObjectOutputStream(mockSocketOutput);
+
+        connectionsSingleton.add(mockOutputStream);
+
+        assertEquals(connectionsSingleton, Server.getConnectionsSingletonInstance());
+    }
+}
