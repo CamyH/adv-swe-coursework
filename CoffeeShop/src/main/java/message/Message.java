c5daf96c2858bb1a2693bc2f291aa87a96e3d3d4
CamyH <cameronhunt88@hotmail.com>
2025-03-28 19:59:53 +0000

    Update message to include auto generated messageID

--- CoffeeShop/src/main/java/message/Message.java
+++ CoffeeShop/src/main/java/message/Message.java
@@ -1,13 +1,33 @@
 package message;
 
 import java.io.Serializable;
+import java.util.Objects;
 import java.util.UUID;
 
 /**
  * Represents a message with a specific type and sender
  */
-public record Message(UUID customerId, String message, MessageType messageType) implements Serializable {
-
+public record Message(UUID messageId, UUID customerId, String message, MessageType messageType) implements Serializable {
+    /**
+     * Constructor to generate a random messageId
+     * @param customerId Id of the customer sending the message
+     * @param message the message body
+     * @param messageType the message type
+     */
+    public Message(UUID customerId, String message, MessageType messageType) {
+        this(UUID.randomUUID(), customerId, message, messageType);
+    }
+    /**
+     * Constructor
+     * @param customerId the customerId cannot be null
+     * @param message the message cannot be null
+     * @param messageType the message cannot be null
+     */
+    public Message {
+        Objects.requireNonNull(customerId);
+        Objects.requireNonNull(message);
+        Objects.requireNonNull(messageType);
+    }
     /**
      * Returns a string representation of the message, including message content,
      * type, and customer Id.
@@ -16,8 +36,9 @@ public record Message(UUID customerId, String message, MessageType messageType) 
      */
     @Override
     public String toString() {
-        return "Message{" +
-                "message='" + message + '\'' +
+        return "{" +
+                "messageId=" + messageId +
+                ", message='" + message + '\'' +
                 ", customerId=" + customerId +
                 ", messageType=" + messageType + '}';
     }
