84e1383ed14fbb8b3831a97b6ceec55e3525fdc2
faiz-k4 <faiz.mohd340@gmail.com>
2025-04-07 09:09:47 +0100

    Adding java doc comments

--- CoffeeShop/src/main/java/utils/SoundPlayer.java
+++ CoffeeShop/src/main/java/utils/SoundPlayer.java
@@ -3,11 +3,25 @@ package utils;
 import javax.sound.sampled.*;
 import java.io.InputStream;
 
+/**
+ * Utility class for playing sound effects in the application.
+ *
+ * @author Mohd Faiz
+ */
 public class SoundPlayer {
+    /**
+     * Enum representing the different types of sounds available in the application.
+     */
     public enum SoundType {
         STARTUP, EXIT, NEW_ITEM_ADD, SUBMIT_ORDER, ORDER_COMPLETE
     }
 
+    /**
+     * Plays the specified sound type.
+     * If the sound file cannot be loaded or found in the application then it will play system beep sound.
+     *
+     * @param type The type of sound to play (from the SoundType enum)
+     */
     public static void playSound(SoundType type) {
         new Thread(() -> {
             try {
@@ -20,7 +34,7 @@ public class SoundPlayer {
                     clip.open(audioIn);
                     clip.start();
                 } else {
-                    java.awt.Toolkit.getDefaultToolkit().beep(); // When there is no sound file
+                    java.awt.Toolkit.getDefaultToolkit().beep();
                 }
             } catch (Exception e) {
                 System.err.println("Error playing sound: " + e.getMessage());
@@ -28,6 +42,12 @@ public class SoundPlayer {
         }).start();
     }
 
+    /**
+     * Maps sound types to their corresponding sound file names.
+     *
+     * @param type The sound type to look up
+     * @return The filename associated with the sound type
+     */
     private static String getSoundFile(SoundType type) {
         return switch (type) {
             case STARTUP -> "Startup.wav";
